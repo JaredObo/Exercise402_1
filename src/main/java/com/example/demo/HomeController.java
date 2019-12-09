ab35ec515a395d488db4ace852e9af10f298704d
Jared Obozele <jared.obozele@gmail.com>
2019-12-09 15:03:45 -0500

    commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
